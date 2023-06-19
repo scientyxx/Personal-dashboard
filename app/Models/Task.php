2662591fa08db52786431763678f465a7d7230ff
SriAliaRosidah <sri.rosidah@students.paramadina.ac.id>
2023-06-19 16:48:07 +0700

    second commit

---
+++ app/Models/Task.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    protected $table = "task";
+    protected $primaryKey = "id";
+    protected $fillable = [
+        'id', 'task_name', 'difficulty_level', 'due-date'
+    ];
+    use HasFactory;
+}
---
+++ database/migrations/2023_06_19_053811_create_tasks_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTasksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('task', function (Blueprint $table) {
+            $table->id();
+            $table->string('task_name', 100);
+            $table->enum('difficulty_level', ['Easy', 'Medium', 'Hard']);
+            $table->string('due_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('task');
+    }
+}
